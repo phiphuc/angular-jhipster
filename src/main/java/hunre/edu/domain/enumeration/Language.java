36530bebe034c7bb4299cf01766b843c62ffdea7
donqu <itsol>
2020-01-01 22:00:16 +0700

    first commit

---
+++ src/main/java/hunre/edu/domain/enumeration/Language.java
@@ -0,0 +1,8 @@
+package hunre.edu.domain.enumeration;
+
+/**
+ * The Language enumeration.
+ */
+public enum Language {
+    FRENCH, ENGLISH, SPANISH
+}
---
+++ src/main/java/hunre/edu/repository/JobHistoryRepository.java
@@ -0,0 +1,15 @@
+package hunre.edu.repository;
+
+import hunre.edu.domain.JobHistory;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the JobHistory entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface JobHistoryRepository extends JpaRepository<JobHistory, Long> {
+
+}
